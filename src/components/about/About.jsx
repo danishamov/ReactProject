bb1296fbe5b07998b1edbbd02b62b08f54a589d9
danishamov <122825234+danishamov@users.noreply.github.com>
2024-08-04 23:20:09 +0100

    Implement About

---
+++ src/components/about/About.jsx
@@ -0,0 +1,48 @@
+export default function About() {
+    return (
+        <div className="aboutus">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 col-sm-12">
+                        <div className="col-md-7 col-sm-7 col-xs-12">
+                            <div className="full">
+                                <h3>SPORTY LEAGUE HISTORY</h3>
+                                <p>
+                                    Voluptate illum dolore ita ipsum, quid
+                                    deserunt singulis, labore admodum ita multos
+                                    malis ea nam nam tamen fore amet. Vidisse
+                                    quid incurreret ut ut possumus transferrem
+                                    si ita labore dolor si appellat, aut dolore
+                                    doctrina. Commodo dolor esse in magna, a a
+                                    multos senserit nam si aliqua iis multos..
+                                </p>
+                                <ul className="icon-list">
+                                    <li>
+                                        <i className="fa fa-angle-right" />{" "}
+                                        Voluptate illum dolore ita ipsum
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-angle-right" />{" "}
+                                        Labore admodum ita multos malis ea nam
+                                        nam tamen fore amet
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-angle-right" />{" "}
+                                        Voluptate illum dolore ita ipsum
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-md-5 col-sm-5 col-xs-12">
+                            <img
+                                className="img-responsive"
+                                src="images/img-07.jpg"
+                                alt="#"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
