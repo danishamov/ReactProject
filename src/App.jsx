b4865b265da34a110eb77b4b0ce165e19afa0b7a
danishamov <122825234+danishamov@users.noreply.github.com>
2024-08-04 23:22:57 +0100

    remove top section
    rename it to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Contact from "./components/contect/Contect";
 import Footer from "./components/footer/Footer";
 import MatchInfo from "./components/matchInfo/MatchInfo";
 import Team from "./components/team/Team";
-import TopSection from "./components/topSection/TopSection";
+import Home from "./components/home/Home";
 import Video from "./components/video/Video";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import News from "./components/news/News";
+import Blog from "./components/blog/Blog";
 
 function App() {
     return (
@@ -18,11 +23,20 @@ function App() {
                     />
                 </div> */}
                 {/* END LOADER */}
-                <TopSection />
-                <MatchInfo />
-                <Contact />
-                <Video />
-                <Team />
+
+                <Header />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/team" element={<Team />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/news" element={<News />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/blog" element={<Blog />} />
+
+                    <Route path="/info" element={<MatchInfo />} />
+                    <Route path="/video" element={<Video />} />
+                </Routes>
                 <Footer />
                 <a href="#home" data-scroll="" className="dmtop global-radius">
                     <i className="fa fa-angle-up" />
