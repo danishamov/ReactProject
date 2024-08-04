608bdff3564cb93508deb5dcacd61427eb1ff57b
danishamov <122825234+danishamov@users.noreply.github.com>
2024-08-04 17:45:31 +0100

    implement video page

---
+++ src/components/video/Video.jsx
@@ -0,0 +1,21 @@
+export default function Video() {
+    return (
+        <section className="video_section_main theme-padding middle-bg vedio">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="full">
+                            <div className="match_vedio">
+                                <img
+                                    className="img-responsive"
+                                    src="images/img-07.jpg"
+                                    alt="#"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
