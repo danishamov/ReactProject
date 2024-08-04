85979ee259ed981db9618f5801318fa878f9f8f2
danishamov <122825234+danishamov@users.noreply.github.com>
2024-08-04 17:44:13 +0100

    implement Contact page

---
+++ src/components/contect/Contect.jsx
@@ -0,0 +1,333 @@
+export default function Contact() {
+    return (
+        <section id="contant" className="contant">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 col-sm-4 col-xs-12">
+                        <aside id="sidebar" className="left-bar">
+                            <div className="banner-sidebar">
+                                <img
+                                    className="img-responsive"
+                                    src="images/img-05.jpg"
+                                    alt="#"
+                                />
+                                <h3>Lorem Ipsum is simply dummy text..</h3>
+                            </div>
+                        </aside>
+                        <h4>Match Fixture</h4>
+                        <aside id="sidebar" className="left-bar">
+                            <div className="feature-matchs">
+                                <div className="team-btw-match">
+                                    <ul>
+                                        <li>
+                                            <img
+                                                src="images/img-01_002.png"
+                                                alt=""
+                                            />
+                                            <span>Portugal</span>
+                                        </li>
+                                        <li className="vs">
+                                            <span>vs</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                src="images/img-02.png"
+                                                alt=""
+                                            />
+                                            <span>Germany</span>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>
+                                            <img
+                                                src="images/img-03_002.png"
+                                                alt=""
+                                            />
+                                            <span>Portugal</span>
+                                        </li>
+                                        <li className="vs">
+                                            <span>vs</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                src="images/img-04_003.png"
+                                                alt=""
+                                            />
+                                            <span>Germany</span>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>
+                                            <img
+                                                src="images/img-05_002.png"
+                                                alt=""
+                                            />
+                                            <span>Portugal</span>
+                                        </li>
+                                        <li className="vs">
+                                            <span>vs</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                src="images/img-06.png"
+                                                alt=""
+                                            />
+                                            <span>Germany</span>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>
+                                            <img
+                                                src="images/img-07_002.png"
+                                                alt=""
+                                            />
+                                            <span>Portugal</span>
+                                        </li>
+                                        <li className="vs">
+                                            <span>vs</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                src="images/img-08.png"
+                                                alt=""
+                                            />
+                                            <span>Germany</span>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>
+                                            <img
+                                                src="images/img-05_002.png"
+                                                alt=""
+                                            />
+                                            <span>Portugal</span>
+                                        </li>
+                                        <li className="vs">
+                                            <span>vs</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                src="images/img-06.png"
+                                                alt=""
+                                            />
+                                            <span>Germany</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </aside>
+                        <h4>Points Table</h4>
+                        <aside id="sidebar" className="left-bar">
+                            <div className="feature-matchs">
+                                <table className="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Team</th>
+                                            <th>P</th>
+                                            <th>W</th>
+                                            <th>L</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-01_004.png"
+                                                    alt=""
+                                                />
+                                                Liverpool
+                                            </td>
+                                            <td>10</td>
+                                            <td>12</td>
+                                            <td>20</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-02_002.png"
+                                                    alt=""
+                                                />
+                                                Chelsea
+                                            </td>
+                                            <td>10</td>
+                                            <td>12</td>
+                                            <td>20</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-03_003.png"
+                                                    alt=""
+                                                />
+                                                Norwich City
+                                            </td>
+                                            <td>20</td>
+                                            <td>15</td>
+                                            <td>20</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-04_002.png"
+                                                    alt=""
+                                                />
+                                                West Brom
+                                            </td>
+                                            <td>60</td>
+                                            <td>10</td>
+                                            <td>60</td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-05.png"
+                                                    alt=""
+                                                />
+                                                sunderland
+                                            </td>
+                                            <td>30</td>
+                                            <td>06</td>
+                                            <td>30</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>
+                                                <img
+                                                    src="images/img-01_004.png"
+                                                    alt=""
+                                                />
+                                                Liverpool
+                                            </td>
+                                            <td>10</td>
+                                            <td>12</td>
+                                            <td>20</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </aside>
+                        <div
+                            className="content-widget top-story"
+                            style={{
+                                background: "url(images/top-story-bg.jpg)",
+                            }}
+                        >
+                            <div className="top-stroy-header">
+                                <h2>
+                                    Top Soccer Headlines Story{" "}
+                                    <a
+                                        href="#"
+                                        className="fa fa-fa fa-angle-right"
+                                    />
+                                </h2>
+                                <span className="date">July 05, 2017</span>
+                                <h2>Other Headlines</h2>
+                            </div>
+                            <ul className="other-stroies">
+                                <li>
+                                    <a href="#">Wenger Vardy won't start</a>
+                                </li>
+                                <li>
+                                    <a href="#">Evans: Vardy just</a>
+                                </li>
+                                <li>
+                                    <a href="#">Pires and Murray </a>
+                                </li>
+                                <li>
+                                    <a href="#">Okazaki backing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Wolfsburg's Rodriguez</a>
+                                </li>
+                                <li>
+                                    <a href="#">Jamie Vardy compared</a>
+                                </li>
+                                <li>
+                                    <a href="#">Arsenal target Mkhitaryan</a>
+                                </li>
+                                <li>
+                                    <a href="#">Messi wins libel case.</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-8 col-sm-8 col-xs-12">
+                        <div className="news-post-holder">
+                            <div className="news-post-widget">
+                                <img
+                                    className="img-responsive"
+                                    src="images/img-01_002.jpg"
+                                    alt=""
+                                />
+                                <div className="news-post-detail">
+                                    <span className="date">20 march 2016</span>
+                                    <h2>
+                                        <a href="blog-detail.html">
+                                            At vero eos et accusamus et iusto
+                                            odio dignissimos ducimus
+                                        </a>
+                                    </h2>
+                                    <p>
+                                        Just hours after that his grandma had
+                                        died, Angel Di Maria imagined how she
+                                        might react if he didn't play
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="news-post-widget">
+                                <img
+                                    className="img-responsive"
+                                    src="images/img-02_003.jpg"
+                                    alt=""
+                                />
+                                <div className="news-post-detail">
+                                    <span className="date">20 march 2016</span>
+                                    <h2>
+                                        <a href="blog-detail.html">
+                                            At vero eos et accusamus et iusto
+                                            odio dignissimos ducimus
+                                        </a>
+                                    </h2>
+                                    <p>
+                                        Just hours after that his grandma had
+                                        died, Angel Di Maria imagined how she
+                                        might react if he didn't play
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="news-post-holder">
+                            <div className="news-post-widget">
+                                <img
+                                    className="img-responsive"
+                                    src="images/img-03_003.jpg"
+                                    alt=""
+                                />
+                                <div className="news-post-detail">
+                                    <span className="date">20 march 2016</span>
+                                    <h2>
+                                        <a href="blog-detail.html">
+                                            At vero eos et accusamus et iusto
+                                            odio dignissimos ducimus
+                                        </a>
+                                    </h2>
+                                    <p>
+                                        Just hours after that his grandma had
+                                        died, Angel Di Maria imagined how she
+                                        might react if he didn't play
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
